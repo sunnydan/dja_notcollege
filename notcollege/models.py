71117fef8de344688064f99a29b822b322a29881
sunnydan <danielhilla@gmail.com>
2017-09-22 11:35:09 -0400

    Removed Page table from database. Made each other table own their own posts instead of having a page that holds onto them for them. Remade database documentation with this in mind.

--- notcollege/models.py
+++ notcollege/models.py
@@ -1,7 +1,6 @@
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
 from django.db import models
-import uuid
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -11,14 +10,12 @@ class Profile(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
     friends = models.ManyToManyField("self")
     liked_books = models.ManyToManyField('Book', related_name="likes")
-    page = models.OneToOneField('Page', related_name="user")
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     isbn = models.PositiveIntegerField()
     authors = models.ManyToManyField(Profile, related_name="books")
-    page = models.OneToOneField('Page', related_name="book")
 
 class Review(models.Model):
     rating = models.PositiveSmallIntegerField()
@@ -32,30 +29,22 @@ class Course(models.Model):
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    teacher = models.ForeignKey(User, related_name="taught_courses")
+    teachers = models.ManyToManyField(User, related_name="taught_courses")
     required_books = models.ManyToManyField(Book, related_name="courses")
     students = models.ManyToManyField(Profile, related_name='joined_courses')
-    page = models.OneToOneField('Page', related_name="course")
 
 class Chatroom(models.Model):
     topic = models.CharField(max_length=255)
     members = models.ManyToManyField(Profile, related_name='chatrooms')
-    page = models.OneToOneField('Page', related_name="chatroom")
 
 class Post(models.Model):
     text = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
-    author = models.ForeignKey(Profile, related_name="posts", null=True)
-    parent_post = models.ForeignKey('Post', related_name="children")
-    review = models.ForeignKey(Review, related_name="posts")
-    page = models.ForeignKey('Page', related_name="posts")
-
-class Page(models.Model):
-    TYPE_CHOICES = (
-        ('USER', 'USER'),
-        ('BOOK', 'BOOK'),
-        ('COURSE', 'COURSE'),
-        ('CHATROOM', 'CHATROOM')
-    )
-    type = models.CharField(max_length=8, choices=TYPE_CHOICES)
+    author = models.ForeignKey(Profile, related_name="authored_posts", null=True)
+    parent_post = models.ForeignKey('Post', related_name="child_posts", null=True)
+    to_profile = models.ForeignKey(Profile, related_name="profile_posts", null=True)
+    review = models.ForeignKey(Review, related_name="review_posts", null=True)
+    chatroom = models.ForeignKey(Chatroom, related_name="chatroom_posts", null=True)
+    course = models.ForeignKey(Course, related_name="course_posts", null=True)
+    book = models.ForeignKey(Book, related_name="book_posts", null=True)
